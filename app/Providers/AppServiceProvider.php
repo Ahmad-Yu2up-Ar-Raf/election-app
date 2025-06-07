1426f06a952ac24f648beb0b1d5aefaacab80707
Ahmad-Yu2up-Ar-Raf <yusufzolldyck@gmail.com>
2025-06-07 11:32:01 +0700

    Fix Laravel constants compatibility

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,23 +24,17 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment(['production', 'staging'])) {
             URL::forceScheme('https');
             
-            // Set semua trusted proxies untuk Railway
-            Request::setTrustedProxies(['*'], Request::HEADER_X_FORWARDED_ALL);
-            
             // Pastikan semua URL dibuat dengan HTTPS
             if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
                 $_SERVER['HTTPS'] = 'on';
                 $_SERVER['SERVER_PORT'] = 443;
             }
             
-            // Force HTTPS untuk semua asset URLs
-            $this->app['url']->forceScheme('https');
-            
             // Set server variables untuk Railway
-            $this->app['request']->server->set('HTTPS', 'on');
-            $this->app['request']->server->set('SERVER_PORT', 443);
-            $this->app['request']->server->set('HTTP_X_FORWARDED_PROTO', 'https');
-            $this->app['request']->server->set('HTTP_X_FORWARDED_PORT', 443);
+            if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
+                $_SERVER['HTTPS'] = 'on';
+                $_SERVER['SERVER_PORT'] = 443;
+            }
             
             // Pastikan session cookies secure
             config([
@@ -49,13 +42,6 @@ class AppServiceProvider extends ServiceProvider
                 'session.http_only' => true,
                 'session.same_site' => 'lax'
             ]);
-        }
-        
-        // Debug untuk melihat environment
-        if (config('app.debug')) {
-            \Log::info('App URL: ' . config('app.url'));
-            \Log::info('Asset URL: ' . config('app.asset_url'));
-            \Log::info('Force HTTPS: ' . config('app.force_https'));
         }
     }
 }
